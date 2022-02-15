a1564d68ea4e3c8d9f8a3479509ebb0382b794f6
Jueff <Jueff@a1.net>
2022-02-15 12:08:59 +0100

    add platformio helper scripts

--- Tools/platformio/installPIO.cmd
+++ Tools/platformio/installPIO.cmd
@@ -9,23 +9,25 @@ if errorlevel 1 goto failed1
 powershell .\installPIO.ps1
 if errorlevel 1 goto failed2
 path=%path%;%USERPROFILE%\.platformio\penv\Scripts
-pause platformio installed
-
+echo platformio installed
+pause
 goto :eof
-
 
 :errorNoPython
 echo.
-pause Error: Python not installed - please visit https://www.python.org/downloads/windows/
+echo Error: Python not installed - please visit https://www.python.org/downloads/windows/
+pause
 start https://www.python.org/downloads/windows/
 exit 1
 
 :failed1
 echo.
-pause Download of installPIO.ps1 failed
+echo Download of installPIO.ps1 failed
+pause
 exit 1
 
 :failed2
 echo.
-pause Execution of installPIO.ps1 failed
+echo Execution of installPIO.ps1 failed
+pause
 exit 1
